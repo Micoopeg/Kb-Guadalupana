e2b5c1abe2f03661ed0e5a5a3555cabe04420d6f
pggteo <pggteo@PGSRVIIS>
2021-04-09 01:02:14 -0600

    Version 1.0 4 sistemas integraids

--- KB_Guadalupana/Controllers/Conexion.cs
+++ KB_Guadalupana/Controllers/Conexion.cs
@@ -16,7 +16,7 @@ namespace KB_Guadalupana.Controllers
 
         public string cadenadeconexiongeneral()
         {
-            string connectionString = @"Server=localhost;Database=bdkbguadalupana;Uid=root;Pwd=;";
+            string connectionString = @"Server=10.60.81.5;Database=bdkbguadalupana;Uid=User4pDes@rrollo;Pwd=BDK0ntr@PG1;";
             return connectionString;
         }
 
